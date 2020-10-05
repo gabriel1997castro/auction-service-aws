ec6830c1a45b060ebc92f92dc1c5c33bf87fb99f
Gabriel Castro <gabriel.castro@coreconsulting.com.br>
2020-10-05 11:00:48 -0300

    common middleware function created

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,10 +1,7 @@
 import { v4 as uuid } from 'uuid';
 import AWS from 'aws-sdk';
-import middy from '@middy/core';
-import httpJsonBodyParser from '@middy/http-json-body-parser';
-import httpEventNormalizer from '@middy/http-event-normalizer';
-import httpErrorHandler from '@middy/http-error-handler';
 import createError from 'http-errors';
+import commonMiddleware from '../lib/commonMiddleware';
 
 const  dynamodb = new AWS.DynamoDB.DocumentClient()
 
@@ -37,9 +34,6 @@ async function createAuction(event, context) {
   };
 }
 
-export const handler = middy(createAuction)
-.use(httpJsonBodyParser())
-.use(httpEventNormalizer())
-.use(httpErrorHandler());
+export const handler = commonMiddleware(createAuction);
 
 // Lambda can run just for 15 minutes
--- src/handlers/getAuctions.js
+++ src/handlers/getAuctions.js
@@ -1,10 +1,7 @@
 
 import AWS from 'aws-sdk';
-import middy from '@middy/core';
-import httpJsonBodyParser from '@middy/http-json-body-parser';
-import httpEventNormalizer from '@middy/http-event-normalizer';
-import httpErrorHandler from '@middy/http-error-handler';
 import createError from 'http-errors';
+import commonMiddleware from '../lib/commonMiddleware';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient()
 
@@ -29,9 +26,6 @@ async function getAuctions(event, context) {
   };
 }
 
-export const handler = middy(getAuctions)
-  .use(httpJsonBodyParser())
-  .use(httpEventNormalizer())
-  .use(httpErrorHandler());
+export const handler = commonMiddleware(getAuctions);
 
 // Lambda can run just for 15 minutes
