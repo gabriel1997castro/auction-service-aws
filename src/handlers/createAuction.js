82e7f0ba582cb78a6f2e445050c5b039237f0f5b
Gabriel Castro <gabriel.castro@coreconsulting.com.br>
2020-11-03 16:16:15 -0300

    body schema verification creating auction

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,7 +1,9 @@
 import { v4 as uuid } from 'uuid';
 import AWS from 'aws-sdk';
 import createError from 'http-errors';
+import validator from '@middy/validator';
 import commonMiddleware from '../lib/commonMiddleware';
+import createAuctionSchema from '../lib/schemas/createAuctionSchema';
 
 const  dynamodb = new AWS.DynamoDB.DocumentClient()
 
@@ -40,6 +42,7 @@ async function createAuction(event, context) {
   };
 }
 
-export const handler = commonMiddleware(createAuction);
+export const handler = commonMiddleware(createAuction)
+  .use(validator({ inputSchema: createAuctionSchema }));
 
 // Lambda can run just for 15 minutes
